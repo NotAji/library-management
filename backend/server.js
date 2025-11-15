80c8c08c11a3766a9064d52bdb8b625d7a4281e4
NotAji <aimoralita23@gmail.com>
2025-11-15 19:16:58 +0800

    connected express to site

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Connected to site...")
+})
+
+app.listen(5000, () => {
+    console.log("server listening on port 5000");
+});
