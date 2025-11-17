d0206c809906538e7fc257bbf3721f7c17d06307
NotAji <aimoralita23@gmail.com>
2025-11-18 02:02:54 +0800

    created the header navbar

---
+++ frontend/javascript/api.js
@@ -0,0 +1,17 @@
+const API_URL = "http://localhost:5000";
+
+async function apiRequest(path, method="GET", body=null, auth=false) {
+    const headers = { "content_type":"application/json" };
+
+    if (auth) {
+        const token = localStorage.getItem("token");
+
+        if (token) headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    const options = { method, headers };
+    if (body) options.body = JSON.stringify(body);
+
+    const res = await fetch(`${API_URL}${path}`, options);
+    return res.json();
+}
